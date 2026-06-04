dd4da10efe85b3da7aca1f53c7446953041ab61b
Vino-ys <yoonsun.yoonsunny@gmail.com>
2026-06-04 16:47:50 +0900

    문제1071 해결

--- problem1071.c
+++ problem1071.c
@@ -1,1 +1,13 @@
 // [기초-반복실행구조]0 입력될 때까지 무한 출력하기1(설명)
+#include <stdio.h>
+int main() {
+	int a = 0;
+	goto notyung;
+	notyung:
+	scanf("%d",&a);
+	if(a == 0) {
+		return 0;
+	}
+	printf("%d\n",a);
+	goto notyung;
+}
