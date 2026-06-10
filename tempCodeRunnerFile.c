2a5c74824d553436a05f3a48101c6f0c35bbd462
Vino-ys <yoonsun.yoonsunny@gmail.com>
2026-06-11 00:01:17 +0900

    문제1084해결

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,13 @@
+int main() {
+    int r,g,b;
+    scanf("%d %d %d",&r, &g, &b);
+
+    for(int i = 0;i < r;i++){
+        for(int j = 0;j < g;j++){
+            for(int k = 0;k < b;k++){
+                printf("%d %d %d\n",i,j,k);
+            }
+        }
+    }
+    return 0;
+}
