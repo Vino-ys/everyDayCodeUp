2ceb0acce0f39ecad31d2e38ae1870a17dc6936a
Vino-ys <yoonsun.yoonsunny@gmail.com>
2026-07-29 22:11:27 +0900

    ezez문제6

---
+++ 여름방학 과제/1주차과제/문제7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+	int n, even = 0, odd = 0;
+	int arr[101];
+
+	scanf("%d", &n);
+	for(int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+	for(int i = 0; i < n; i++) {
+		if(arr[i] % 2 == 0) even += arr[i];
+		else odd += arr[i];
+	}
+
+	printf("%d %d", even, odd);
+	
+	return 0;
+}
