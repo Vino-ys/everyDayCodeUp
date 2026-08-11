d7a1786c76c3ebecdb2062967e4f5d1fbc6d3684
Vino-ys <yoonsun.yoonsunny@gmail.com>
2026-08-11 17:12:16 +0900

    7

--- 여름방학 과제/2주차과제/문제7.py
+++ 여름방학 과제/2주차과제/문제7.py
@@ -1,1 +1,17 @@
-# 풀어끄악
+count = 0
+n = int(input())
+
+firstSet = set(input().split())
+
+n = int(input())
+
+secondSet = set(input().split())
+
+result = sorted(firstSet & secondSet)
+
+for _ in result:
+	count += 1
+	
+print(count)
+for i in result:
+	print(i, end = " ")
