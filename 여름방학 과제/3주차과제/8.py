35e885ef59acc2bd0899a4515c32da84e1d820e6
Vino-ys <yoonsun.yoonsunny@gmail.com>
2026-08-18 04:57:09 +0900

    8

---
+++ 여름방학 과제/3주차과제/8.py
@@ -0,0 +1,27 @@
+n = int(input())
+stack = []
+list = []
+
+for _ in range(n):
+	list.append(input().split())
+
+for i in list:
+	commend = i[0]
+	
+	if commend == 'PUSH':
+		stack.append(i[1])
+		
+	elif commend == 'SIZE':
+		print(len(stack))
+		
+	elif commend == 'POP':
+		if len(stack) == 0:
+			print('-1')
+		else:
+			print(stack.pop())
+	elif commend == 'FRONT':
+		if len(stack) == 0:
+			print('-1')
+		else:
+			print(stack[0])
+			stack.append(stack.pop(0))
